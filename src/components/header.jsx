852edc9c8d53acceb5126bace48d86dfd0a7094f
Ankit Kante <ankit.kante@gmail.com>
2025-10-25 14:38:23 +0530

    Added logo and header component

---
+++ src/components/header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="bg-black text-white p-4">
+      <img src="/logo.png" alt="Logo" className="h-12 w-12 inline-block mr-2" />
+    </header>
+  );
+}
