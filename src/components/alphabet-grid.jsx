9e06f4b7bb7bbcdafd9d0df2a73355461d3c19fd
Ankit Kante <ankit.kante@gmail.com>
2025-11-04 15:47:17 +0530

    Use correct library. use that to animate hover and click effects.

--- src/components/alphabet-grid.jsx
+++ src/components/alphabet-grid.jsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/navigation";
+import { motion } from "motion/react"
 
 export default function AlphabetGrid({
   setSelectedAlphabet
@@ -11,13 +12,15 @@ export default function AlphabetGrid({
     <div className="font-sans flex flex-col items-center justify-center p-4 gap-3 sm:p-8">
       <div className="grid grid-cols-4 sm:grid-cols-6 md:grid-cols-8 gap-2 sm:gap-3">
         {letters.map((letter) => (
-          <button
+          <motion.button
             key={letter}
             onClick={() => router.push(`/alphabet/${letter}`)}
-            className="w-12 h-12 sm:w-14 sm:h-14 flex items-center justify-center border rounded-lg hover:bg-gray-100 text-xl sm:text-2xl"
+            whileHover={{scale: 1.1}}
+            whileTap={{scale: 0.9}}
+            className="w-12 h-12 sm:w-14 sm:h-14 flex items-center justify-center border rounded-lg hover:bg-black-100 text-xl sm:text-2xl"
           >
             {letter}
-          </button>
+          </motion.button>
         ))}
       </div>
     </div>
