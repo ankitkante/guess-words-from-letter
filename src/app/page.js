eadc833184c2c2657b4adf9f0d41bf60869b36cf
Ankit Kante <ankit.kante@gmail.com>
2025-11-25 14:32:51 +0530

    Use dark theme to change the bg color of entire app.

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,9 @@
 'use client';
 import AlphabetGrid from "@/components/alphabet-grid";
-import WordInput from "@/components/word-input";
-import WordList from "@/components/word-list";
 
 export default function Home() {
   return (
-    <div className="flex flex-col bg-black h-[calc(100vh-64px)] text-white">
+    <div className="flex flex-col h-[calc(100vh-64px)]">
       <div className="flex flex-col items-center justify-center h-full gap-4 p-4">
         <h2 className="text-2xl">Select an alphabet</h2>
         <AlphabetGrid></AlphabetGrid>
